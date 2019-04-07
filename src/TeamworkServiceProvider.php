57f48ca06aef24361023723ff913813c92991b5b
robert <robert@thebug.ro>
2019-04-07 13:00:02 +0300

    Update TeamworkServiceProvider.php

--- src/TeamworkServiceProvider.php
+++ src/TeamworkServiceProvider.php
@@ -4,42 +4,41 @@ namespace DigitalEquation\Teamwork;
 
 use DigitalEquation\Teamwork\Console\InstallCommand;
 use DigitalEquation\Teamwork\Console\PublishCommand;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class TeamworkServiceProvider extends ServiceProvider
 {
-	/**
-	 * Bootstrap the application services.
-	 */
-	public function boot()
-	{
-		/*
-		 * Optional methods to load the package assets.
-		 */
-		if ($this->app->runningInConsole()) {
-			$this->publishes([
-				__DIR__ . '/../config/config.php' => config_path('teamwork.php'),
-			], 'config');
-		}
-	}
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load the package assets.
+         */
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('teamwork.php'),
+            ], 'config');
+        }
+    }
 
-	/**
-	 * Register the application services.
-	 */
-	public function register()
-	{
-		// Automatically apply the package configuration
-		$this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'teamwork');
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'teamwork');
 
-		$this->commands([
-			InstallCommand::class,
-			PublishCommand::class,
-		]);
+        $this->commands([
+            InstallCommand::class,
+            PublishCommand::class,
+        ]);
 
-		// Register the main class to use with the facade
-		$this->app->singleton('teamwork', function () {
-			return new Teamwork;
-		});
-	}
+        // Register the main class to use with the facade
+        $this->app->singleton('teamwork', function () {
+            return new Teamwork;
+        });
+    }
 }

cf6876740d696420eb6583a0977ec657da26bdcb
Cristian S <serbancristian1995@gmail.com>
2020-04-03 16:37:15 +0300

    - removed DetectsApplicationNamespace

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -4,11 +4,9 @@ namespace DigitalEquation\Teamwork\Console;
 
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
-use Illuminate\Console\DetectsApplicationNamespace;
 
 class InstallCommand extends Command
 {
-    use DetectsApplicationNamespace;
 
     /**
      * The name and signature of the console command.

934bc4be8f0c166223e2b3003393ace73dc4e83a
Robert-Cristian Chiribuc <robert.chiribuc@thebug.ro>
2021-07-15 08:42:03 +0300

    [Teamwork.php] add method to set credentials + misc

--- src/Teamwork.php
+++ src/Teamwork.php
@@ -9,49 +9,46 @@ use GuzzleHttp\Client;
 
 class Teamwork
 {
-    /**
-     * @var \GuzzleHttp\Client
-     */
-    protected Client $client;
+    private ?string $apiKey = null;
 
-    /**
-     * ApiClient constructor.
-     */
-    public function __construct()
-    {
-        $this->client = new Client([
-            'base_uri' => sprintf('https://%s.teamwork.com/desk/v1/', config('teamwork.desk.domain')),
-            'auth'     => [config('teamwork.desk.key'), ''],
-        ]);
-    }
+    private ?string $domain = null;
 
-    /**
-     * Teamwork Desk.
-     *
-     * @return Desk
-     */
     public function desk(): Desk
     {
-        return new Desk($this->client);
+        return new Desk($this->client());
     }
 
-    /**
-     * Teamwork HelpDocs.
-     *
-     * @return HelpDocs
-     */
     public function helpDocs(): HelpDocs
     {
-        return new HelpDocs($this->client);
+        return new HelpDocs($this->client());
     }
 
-    /**
-     * Teamwork Tickets.
-     *
-     * @return Tickets
-     */
     public function tickets(): Tickets
     {
-        return new Tickets($this->client);
+        return new Tickets($this->client());
+    }
+
+    public function setCredentials(string $apiKey, string $domain): void
+    {
+        $this->apiKey = $apiKey;
+        $this->domain = $domain;
+    }
+
+    private function getCredentials(): array
+    {
+        return [
+            'api_key' => $this->apiKey ?: config('teamwork.desk.key'),
+            'domain'  => $this->domain ?: config('teamwork.desk.domain'),
+        ];
+    }
+
+    private function client(): Client
+    {
+        $credentials = $this->getCredentials();
+
+        return new Client([
+            'base_uri' => sprintf('https://%s.teamwork.com/desk/v1/', $credentials['domain']),
+            'auth'     => [$credentials['api_key'], ''],
+        ]);
     }
 }
